0e6e65b24c29dc6062cb27809ae64731db869d91
Mauro Brandan <maurobrandan.mb@gmail.com>
2021-06-04 13:55:05 -0300

    Eventos y manejo del estado en la pagina BadgeNew

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+import './styles/BadgeForm.css'
+
+export class BadgeForm extends Component {
+	handleClick = (e) => {
+		console.log('Button was clicked')
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		console.log('Form was submitted')
+	}
+
+	render() {
+		return (
+			<div>
+				<h1 className='form__title'>New Attendant</h1>
+
+				<form onSubmit={this.handleSubmit} className='form__container'>
+					<label>First Name</label>
+					<input
+						onChange={this.props.onChange}
+						type='text'
+						name='firstName'
+						value={this.props.formValues.firstName}
+					/>
+
+					<label>Last Name</label>
+					<input
+						onChange={this.props.onChange}
+						type='text'
+						name='lastName'
+						value={this.props.formValues.lastName}
+					/>
+
+					<label>Email</label>
+					<input
+						onChange={this.props.onChange}
+						type='email'
+						name='email'
+						value={this.props.formValues.email}
+					/>
+
+					<label>Job Title</label>
+					<input
+						onChange={this.props.onChange}
+						type='text'
+						name='jobTitle'
+						value={this.props.formValues.jobTitle}
+					/>
+
+					<label>Twitter</label>
+					<input
+						onChange={this.props.onChange}
+						type='text'
+						name='twitterAccount'
+						value={this.props.formValues.twitterAccount}
+					/>
+
+					<button onClick={this.handleClick} className='btn btn-primary'>
+						Save
+					</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default BadgeForm
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 import './styles/BadgeNew.css'
+import Hero from '../images/badge-header.svg'
 import Navbar from '../components/Navbar'
 import Badge from '../components/Badge'
-import Hero from '../images/badge-header.svg'
+import BadgeForm from '../components/BadgeForm'
 
 class BadgeNew extends React.Component {
+	state = {
+		form: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			jobTitle: '',
+			twitterAccount: '',
+		},
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			form: {
+				...this.state.form,
+				[e.target.name]: e.target.value,
+			},
+		})
+	}
+
 	render() {
 		return (
 			<div>
@@ -13,14 +33,16 @@ class BadgeNew extends React.Component {
 				<div className='BadgeNew__hero'>
 					<img className='BadgeNew__hero--img' src={Hero} alt='Hero' />
 				</div>
-				<div className='container'>
+				<div className='BadgeNew__main'>
 					<Badge
-						firstName='Mauro'
-						lastName='Brandan'
-						jobTitle='Frontend Developer'
-						twitterAccount='maurobrandan'
+						firstName={this.state.form.firstName}
+						lastName={this.state.form.lastName}
+						jobTitle={this.state.form.jobTitle}
+						twitterAccount={this.state.form.twitterAccount}
+						email={this.state.form.email}
 						avatarUrl='https://es.gravatar.com/userimage/177088679/ede52fae594be9df19c45c39347a88eb.jpg?size=200'
 					/>
+					<BadgeForm onChange={this.handleChange} formValues={this.state.form} />
 				</div>
 			</div>
 		)
